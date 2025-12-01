fda869add62964cee1ce366c788f8b0ba0921c84
Artur <artur.czerwiak@gmail.com>
2025-12-01 09:41:37 +0100

    Add throwing error when min > max

--- 03/app.js
+++ 03/app.js
@@ -2,6 +2,9 @@ export default function randomNumber(min, max) {
 	if (typeof min !== 'number' || typeof max !== 'number') {
 		throw new Error('Scope schould be expressed as a number');
 	}
+	if (min > max) {
+		throw new Error('Max schould be greater than min');
+	}
 	const number = Math.random() * (max - min) + min;
 	return Math.round(number);
 }
