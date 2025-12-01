80228ee80752f68f4ddba9699580533cfc14773c
Artur <artur.czerwiak@gmail.com>
2025-12-01 10:11:29 +0100

    Task 04 completed

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,35 @@
 export default class User {
+	constructor({ email, password }) {
+		this.setEmail(email);
+		this.setPassword(password);
+	}
 
+	setEmail(email) {
+		if (!email.includes('@')) {
+			throw new Error('Email is incorrect');
+		}
+		this.email = email;
+	}
+
+	setPassword(pass) {
+		if (pass.length < 8) {
+			throw new Error('Password is incorrect');
+		}
+		this.password = pass;
+	}
+
+	getEmail() {
+		return this.email;
+	}
+
+	getPassword() {
+		return this.password;
+	}
+
+	login() {
+		if (this.email.includes('devmentor.pl')) {
+			return true;
+		}
+		return false;
+	}
 }
