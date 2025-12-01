825eebd5be7fa154b59392fb54c1c077cb89c6d1
Artur <artur.czerwiak@gmail.com>
2025-12-01 09:38:22 +0100

    Add throwing error when scope is not a number

--- 03/app.js
+++ 03/app.js
@@ -1,4 +1,7 @@
 export default function randomNumber(min, max) {
+	if (typeof min !== 'number' || typeof max !== 'number') {
+		throw new Error('Scope schould be expressed as a number');
+	}
 	const number = Math.random() * (max - min) + min;
 	return Math.round(number);
 }
