183656e0533b86b7bd95d8032358197fb56ca077
Artur <artur.czerwiak@gmail.com>
2025-12-01 09:37:32 +0100

    Create test for throwing error

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,3 +4,10 @@ it('When scope 1 - 1 return 1', () => {
 	const number = randomNumber(1, 1);
 	expect(number).toBe(1);
 });
+
+it('When in scope passed nor number throw error', () => {
+	function number() {
+		randomNumber('a', 1);
+	}
+	expect(number).toThrow();
+});
